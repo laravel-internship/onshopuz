043890653fe9892dd293228a98bb6e2bfa67308c
Azimbek <azimbekxaydarov946@gmail.com>
2021-10-14 19:40:46 +0500

    page(card,detail),migrate(order,orderdetalist,wishlist),model(order,orderdetalist,wishlist),seed(category)

---
+++ app/Http/Livewire/DetailComonent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DetailComonent extends Component
+{
+    public function render()
+    {
+        return view('livewire.detail-comonent')->layout('layouts.base');
+    }
+}
---
+++ database/migrations/2021_10_14_125415_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('all_price');
+            $table->date('date');
+            $table->decimal('delivery_price');
+            $table->integer('status');
+            $table->string('payment_type');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
