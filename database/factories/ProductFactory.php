2f0b03396ccf2582f262b6abfa90174e6152759f
Farrukh <choriyevfarrux997@gmail.com>
2021-10-22 19:58:42 +0500

    add past changes

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -27,19 +27,21 @@ class ProductFactory extends Factory
         $about = $this->faker->unique()->words($nb = 10, $asText = true);
         $description = $this->faker->unique()->words($nb = 80, $asText = true);
         $slug = \Str::slug($name);
-        $price = $this->faker->numberBetween(1000, 100000000);
-        $quentity = $this->faker->numberBetween(1, 1000);
-        $discount = $this->faker->numberBetween(1, 800);
+        $price = $this->faker->numberBetween(10, 1000);
+        $quentity = $this->faker->numberBetween(1, 100);
+        $discount = $this->faker->numberBetween(1, 100);
         $category=$this->faker->numberBetween(1, 10);
-        $image = $this->faker->imageUrl(300, 300);
+        $image = 'digital_'.str_pad($this->faker->numberBetween(1,22), 2, 0, STR_PAD_LEFT).'.jpg';
+        $images = ['digital_'.str_pad($this->faker->numberBetween(1,22), 2, 0, STR_PAD_LEFT).'.jpg', 'digital_'.str_pad($this->faker->numberBetween(1,22), 2, 0, STR_PAD_LEFT).'.jpg', 'digital_'.str_pad($this->faker->numberBetween(1,22), 2, 0, STR_PAD_LEFT).'.jpg'];
         $status = $this->faker->numberBetween(0, 1);
+        $images = implode(',', $images);
         return [
             'name' => $name,
             'price' => $price,
             'quatity' => $quentity,
             'category_id'=>$category,
             'image' => $image,
-            'images' => $image,
+            'images' => $images,
             'status' => $status,
             'data'=>$name,
             'discount'=>$discount,
